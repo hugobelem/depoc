a6edcaf87b5b20c1df2104fde453258ff958264f
Hugo Belém <belem.hugo@hotmail.com>
2024-12-15 17:29:22 -0300

    removed djoser url paths

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -4,7 +4,4 @@ from django.urls import path, re_path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^auth/', include('djoser.urls')),
-    re_path(r'^auth/', include('djoser.urls.authtoken')),
-    re_path(r'^auth/', include('djoser.urls.jwt')),
 ]
