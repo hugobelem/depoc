e4b28497164d0e927b0eab4296db6c3c38fda286
Hugo Belém <belem.hugo@hotmail.com>
2024-12-15 15:46:02 -0300

    added jwt auth

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
+    re_path(r'^auth/', include('djoser.urls.jwt')),
 ]
