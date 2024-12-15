476259a7fe17180a3dc3772ce817fd37a63df811
Hugo Belém <belem.hugo@hotmail.com>
2024-12-15 19:31:15 -0300

    added jwt token and refresh urls path

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -3,8 +3,12 @@ from django.urls import path
 
 from . import views
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.get_routes, name='get_routes'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
