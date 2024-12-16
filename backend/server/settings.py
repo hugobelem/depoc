48873255aeb7cc73d068258a406e0fef1e471582
Hugo Belém <belem.hugo@hotmail.com>
2024-12-16 14:48:12 -0300

    changed to postgres database

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 from datetime import timedelta
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -98,11 +100,18 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost:5432/depoc',
+        conn_max_age=600
+    )
 }
 
 
