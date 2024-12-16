22c2f07f17d730bcf77f176ad4678b58568f8bbb
Hugo Belém <belem.hugo@hotmail.com>
2024-12-15 21:46:11 -0300

    removed server view authentication classes decorator

--- backend/server/views.py
+++ backend/server/views.py
@@ -1,15 +1,9 @@
 from rest_framework import permissions
 from rest_framework.response import Response
-from rest_framework.decorators import (
-    api_view,
-    permission_classes,
-    authentication_classes,
-)
-from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import api_view, permission_classes
 
 
 @api_view(['GET'])
-@authentication_classes([TokenAuthentication])
 @permission_classes([permissions.AllowAny])
 def get_routes(request):
     routes = [
