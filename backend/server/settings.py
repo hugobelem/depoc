a445183c4ed5d511304e1b4396e1acadc7d83b64
Hugo Belém <belem.hugo@hotmail.com>
2024-12-16 19:23:41 -0300

    fixed settings deguc environ condition

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEPOC', 'False').lower() == "true"
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ') #type: ignore
 
