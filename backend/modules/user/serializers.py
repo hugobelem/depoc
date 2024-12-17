98b5ade300cca16a4f00780e417252575f91289c
Hugo Belém <belem.hugo@hotmail.com>
2024-12-17 01:59:17 -0300

    added password serializer field to UserSerializer

--- backend/modules/user/serializers.py
+++ backend/modules/user/serializers.py
@@ -5,18 +5,12 @@ User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+
     class Meta:
         model = User
-        fields = [
-            'id',
-            'name',
-            'email',
-            'username',
-            'password',
-        ]
-        extra_kwargs = {'password': {'write_only': True},}
+        fields = ['id', 'name', 'email', 'username', 'password']
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
-    
