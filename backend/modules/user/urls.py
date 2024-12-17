a502980d7a7e0768faec11f39e5b260e18a9a408
Hugo Belém <belem.hugo@hotmail.com>
2024-12-17 00:29:33 -0300

    updated get_me view to GetMe class

--- backend/modules/user/urls.py
+++ backend/modules/user/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.me, name='me'),
+    path('', views.GetMe.as_view(), name='me'),
 ]
--- backend/modules/user/views.py
+++ backend/modules/user/views.py
@@ -1,13 +1,46 @@
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import permissions
+
+from django.contrib.auth import get_user_model
 
 from .serializers import UserSerializer
 
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def me(request):
-    user = request.user
-    serializer = UserSerializer(user)
-    return Response(serializer.data)
+User = get_user_model()
+
+
+class GetMe(APIView):
+    """
+    View to get logged in user info.
+
+    * Requires token authentication.
+    """
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, format=None):
+        """
+        Return information of logged in user.
+        """
+        user = request.user
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    def put(self, request, format=None):
+        """
+        Update information of logged in user.
+        """        
+        data = request.data
+        default_attrs = ['name', 'email', 'username']
+        data_attrs = [field for field in data.keys()]
+        matching_attrs = all(item in default_attrs for item in data_attrs)
+
+        if not data_attrs:
+            return Response(f'attributes can not be empty')
+        elif matching_attrs:
+            user = User.objects.filter(id=request.user.id)
+            update = {key: value for key, value in data.items()}
+            user.update(**update)
+            return Response('Ok')
+        else:
+            wrong_attrs = [item for item in data_attrs if item not in default_attrs]
+            return Response(f'no matching attributes: {wrong_attrs}')         
