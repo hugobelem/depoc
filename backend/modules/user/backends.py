c19ba942cb79d9acfceda1bb63f99207e2bcf97c
Hugo Belém <belem.hugo@hotmail.com>
2024-12-19 10:07:55 -0300

    added line space to backends.py

--- backend/modules/user/backends.py
+++ backend/modules/user/backends.py
@@ -2,6 +2,7 @@ from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
 from django.db import models
 
+
 class EmailOrUsernameBackend(ModelBackend):
     '''
     Allow users to authenticate using both email or username.
