36ad014dbebca3214a0aba9a606b4554b8ad9304
Hugo Belém <belem.hugo@hotmail.com>
2024-12-20 16:01:15 -0300

    removed unused imports

--- backend/modules/business/views.py
+++ backend/modules/business/views.py
@@ -3,7 +3,6 @@ from rest_framework.response import Response
 from rest_framework import permissions
 from rest_framework import status
 
-from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 
 from .serializers import BusinessSerializer
