2e5b5c7d845bc4808c784045195dd76c803d2520
Hugo Belém <belem.hugo@hotmail.com>
2024-12-20 09:47:06 -0300

    registered business models to admin

--- backend/modules/business/admin.py
+++ backend/modules/business/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from modules.business.models import Business, BusinessOwner
 
+
+class BusinessAdmin(admin.ModelAdmin):
+    list_display = ('id', 'tradeName', 'registrationNumber')
+
+class BusinessOwnerAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'business')
+
+
+admin.site.register(Business, BusinessAdmin)
+admin.site.register(BusinessOwner, BusinessOwnerAdmin)
