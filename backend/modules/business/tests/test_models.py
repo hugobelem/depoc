cd888af78600d8cbbfc26ce628c8b91bc4f6529a
Hugo Belém <belem.hugo@hotmail.com>
2024-12-20 17:17:16 -0300

    updated business tests

--- backend/modules/business/tests/test_models.py
+++ backend/modules/business/tests/test_models.py
@@ -1,26 +1,55 @@
 from django.test import TestCase
-from django.contrib.auth import authenticate, get_user_model
+from django.contrib.auth import get_user_model
 from django.db.utils import IntegrityError
 
 from modules.business.models import Business, BusinessOwner
+
+import ulid
+
 
 User = get_user_model()
 
 class BusinessModelTest(TestCase):
     def setUp(self):
-        return super().setUp()
+        self.business = Business.objects.create(
+            id='01JFJT8RTWQBMQJ6CGEE2Q0M77',
+            legalName='Business Inc',
+            tradeName='Biz',
+            registrationNumber='01234567891234',
+            stateRegistration='',
+            cityRegistration='',
+            companyType='',
+            streetAddress='',
+            addressNumber='',
+            neighborhood='',
+            city='',
+            state='',
+            postCode='',
+            phone='',
+            email='',
+            category='',
+            active=True
+        )
     
 
     def test_business_creation(self):
-        ...
+        self.assertEqual(self.business.legalName, 'Business Inc')
+        self.assertEqual(self.business.tradeName, 'Biz')
+        self.assertEqual(self.business.registrationNumber, '01234567891234')
 
 
-    def test_required_fields(self):
-        ...
+    def test_missing_required_fields(self):
+        with self.assertRaises(IntegrityError):
+            Business.objects.create(
+                legalName='Business2 Inc',
+                tradeName='Biz2',
+                registrationNumber=None,
+            )
 
 
     def test_optional_fields(self):
-        ...
+        self.assertEqual(self.business.stateRegistration, '')
+        self.assertEqual(self.business.cityRegistration, '')
 
 
     def test_unique_registration_number(self):
