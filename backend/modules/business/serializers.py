348f3db5cdf9d9a85db0e1279ec9d86d8d6a3915
Hugo Belém <belem.hugo@hotmail.com>
2024-12-20 12:00:40 -0300

    created BusinessSerializer

--- backend/modules/business/serializers.py
+++ backend/modules/business/serializers.py
@@ -1,4 +1,38 @@
 from rest_framework import serializers
 
+from .models import Business, BusinessOwner # type: ignore
 
 
+class BusinessSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Business
+        fields = [
+            'id',
+            'legalName',
+            'tradeName',
+            'registrationNumber',
+            'stateRegistration',
+            'cityRegistration',
+            'companyType',
+            'category',
+            'streetAddress',
+            'addressNumber',
+            'neighborhood',
+            'city',
+            'state',
+            'postCode',
+            'phone',
+            'email',            
+        ]
+        required = ['legalName', 'tradeName', 'registrationNumber']
+        
+    
+    def create(self, validated_data):
+        business = Business.objects.create(**validated_data)
+        return business
+    
+    
+    def update(self, instance, validated_data):
+        Business.objects.filter(id=instance.id).update(**validated_data)
+        instance.refresh_from_db()
+        return instance
