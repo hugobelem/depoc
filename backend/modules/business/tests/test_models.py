d1f65865175483e53dc532591292b6dc82f0badc
Hugo Belém <belem.hugo@hotmail.com>
2024-12-20 16:41:15 -0300

    updated business test

--- backend/modules/business/tests/test_models.py
+++ backend/modules/business/tests/test_models.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
+from django.contrib.auth import authenticate, get_user_model
+from django.db.utils import IntegrityError
 
-# Create your tests here.
+from modules.business.models import Business, BusinessOwner
+
+User = get_user_model()
+
+class BusinessModelTest(TestCase):
+    def setUp(self):
+        return super().setUp()
+    
+
+    def test_business_creation(self):
+        ...
+
+
+    def test_required_fields(self):
+        ...
+
+
+    def test_optional_fields(self):
+        ...
+
+
+    def test_unique_registration_number(self):
+        ... 
+
+
+    def test_access_to_inactive_business(self):
+        ...         
+    
+
+class BusinessOwnerTest(TestCase):
+    def setUp(self):
+        ...
+    
+
+    def test_business_owner_creation(self):
+        ...    
+    
+
+    def test_one_to_one_constraints(self):
+        ...    
