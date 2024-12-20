5fc1af995de2a418fb0f023841ce4e8288c8c57d
Hugo Belém <belem.hugo@hotmail.com>
2024-12-20 15:41:48 -0300

    added biz patch logic

--- backend/modules/business/views.py
+++ backend/modules/business/views.py
@@ -69,19 +69,53 @@ class BusinessEndpoint(APIView):
     def get(self, request):
         try:
             owner = get_object_or_404(BusinessOwner, owner=request.user)
+            business = get_object_or_404(Business, id=owner.business.id)
+            serializer = BusinessSerializer(business)            
         except:
             return Response(
                 {'error': 'Owner does not have a registered business.'},
                 status=status.HTTP_404_NOT_FOUND
             )
         
-        business = get_object_or_404(Business, id=owner.business.id)
-        serializer = BusinessSerializer(business)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
     def patch(self, request):
-        ...
+        data = request.data
+
+        try:
+            owner = get_object_or_404(BusinessOwner, owner=request.user)
+            business = get_object_or_404(Business, id=owner.business.id)
+        except:
+            return Response(
+                {'error': 'Owner does not have a registered business.'},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        
+        if not data:
+            return Response(
+                {'error': 'No data provided for business creation.'},
+                status=status.HTTP_400_BAD_REQUEST                
+            )
+        
+        field_errors = self.check_field_errors(request)
+        if field_errors:
+            return Response(
+                {
+                    'error': f'Invalid fields: {", ".join(field_errors)}',
+                    'expected': BusinessSerializer.Meta.expected
+                }, 
+                status=status.HTTP_400_BAD_REQUEST)   
+        
+        serializer = BusinessSerializer(instance=business, data=data, partial=True)
+        if not serializer.is_valid():
+            return Response(
+                {'error': 'Validation failed', 'details': serializer.errors},
+                status=status.HTTP_400_BAD_REQUEST
+            )           
+        
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)         
 
 
     def delete(self, request):
