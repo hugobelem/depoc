d340b7c5ebf27cf119af630c23e3723c631745e2
Hugo Belém <belem.hugo@hotmail.com>
2024-12-22 20:34:41 -0300

    removed unused imports

--- backend/modules/members/views.py
+++ backend/modules/members/views.py
@@ -3,9 +3,6 @@ from rest_framework.response import Response
 from rest_framework import permissions
 from rest_framework import status
 
-from django.shortcuts import get_object_or_404
-
-from modules.members.models import Members, MembersCredentials
 from .serializers import MemberSerializer
 
 
