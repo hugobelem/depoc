a50e66e84b7114344150156046d9c028e0c082fb
Hugo Belém <belem.hugo@hotmail.com>
2024-12-22 19:28:59 -0300

    added Members model to admin

--- backend/modules/members/admin.py
+++ backend/modules/members/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 
+from .models import Members
 
 
+class MembersAdmin(admin.ModelAdmin):
+    list_display = ['id', 'firstName', 'role', 'status']
+
+
+admin.site.register(Members, MembersAdmin)
