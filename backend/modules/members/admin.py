66f2dda33c68b5bc2b18b95a183ef3e983a4603c
Hugo Belém <belem.hugo@hotmail.com>
2024-12-22 19:42:45 -0300

    added members credentials to admin panel

--- backend/modules/members/admin.py
+++ backend/modules/members/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 
-from .models import Members
+from .models import Members, MembersCredentials
 
 
 class MembersAdmin(admin.ModelAdmin):
     list_display = ['id', 'firstName', 'role', 'status']
 
+class MembersCredentialsAdmin(admin.ModelAdmin):
+    list_display = ['member', 'credentials']
+
 
 admin.site.register(Members, MembersAdmin)
+admin.site.register(MembersCredentials, MembersCredentialsAdmin)
