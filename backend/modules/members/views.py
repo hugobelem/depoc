5f55dc1db4343e0737d3bea3fa192b40cc8fd817
Hugo Belém <belem.hugo@hotmail.com>
2024-12-22 22:37:15 -0300

    added patch and delete to MembersEndpoint

--- backend/modules/members/views.py
+++ backend/modules/members/views.py
@@ -3,7 +3,10 @@ from rest_framework.response import Response
 from rest_framework import permissions
 from rest_framework import status
 
+from django.shortcuts import get_object_or_404
+
 from .serializers import MemberSerializer
+from .models import Members
 
 
 class MembersEndpoint(APIView):
@@ -49,3 +52,66 @@ class MembersEndpoint(APIView):
         
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)   
+
+
+    def patch(self, request, id=None):
+        try:
+            owner = request.user
+            owner.business         
+        except:
+            message = 'Owner does not have a registered business.'
+            return Response({'error': message}, status=status.HTTP_404_NOT_FOUND) 
+
+        try:
+            member = Members.objects.get(id=id)    
+        except:
+            message = 'Member not found.'
+            return Response({'error': message}, status=status.HTTP_404_NOT_FOUND)          
+
+        data = request.data
+        if not data:
+            message = 'No data provided for member update.'
+            return Response({'error': message}, status=status.HTTP_400_BAD_REQUEST)
+        
+        field_errors = self.check_field_errors(request)
+        if field_errors:
+            message = f'Invalid fields: {", ".join(field_errors)}'
+            expected_fields = MemberSerializer.Meta.fields
+            return Response(
+                {'error': message, 'expected': expected_fields}, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+         
+        serializer = MemberSerializer(
+            instance=member, 
+            data=data,
+            partial=True,
+            context={'member':member}
+        )
+        if not serializer.is_valid():
+            return Response(
+                {'error': 'Validation failed', 'details': serializer.errors},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+
+    def delete(self, request, id):
+        try:
+            owner = request.user
+            owner.business         
+        except:
+            message = 'Owner does not have a registered business.'
+            return Response({'error': message}, status=status.HTTP_404_NOT_FOUND) 
+
+        try:
+            member = Members.objects.get(id=id)    
+        except:
+            message = 'Member not found.'
+            return Response({'error': message}, status=status.HTTP_404_NOT_FOUND)
+
+        member.delete()
+        message = 'The member has been successfully deleted'
+        return Response({'success:': message}, status=status.HTTP_200_OK)
