26c2f8ce1c1010a8ba864078fad871001f6f2548
Hugo Belém <belem.hugo@hotmail.com>
2024-12-26 19:24:31 -0300

    renamed members test classes

--- backend/modules/members/tests/test_models.py
+++ backend/modules/members/tests/test_models.py
@@ -3,12 +3,13 @@ from django.test import TestCase
 from . import factories
 
 
-class TestMembersModes(TestCase):
+class TestMembersModel(TestCase):
     def test_str_return(self):
         member = factories.MembersFactory(firstName='The', lastName='Member')
         self.assertEqual(member.__str__(), 'The Member')
 
-class TestMembersCredentials(TestCase):
+
+class TestMembersCredentialsModel(TestCase):
     def test_str_return(self):
         member = factories.MembersFactory(firstName='My', lastName='Member')
         credential = factories.CredentialsFactory(email='mymember@email.com')
