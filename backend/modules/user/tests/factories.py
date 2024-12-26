a3b5c4152b1afa797bb69725d1b438a4a9e948dd
Hugo Belém <belem.hugo@hotmail.com>
2024-12-26 18:11:00 -0300

    added factory boy to user tests

---
+++ backend/modules/user/tests/factories.py
@@ -0,0 +1,24 @@
+import factory
+
+import ulid
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+def faker(provider):
+    return factory.Faker(provider=provider, locale='pt_BR')
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    id = factory.Sequence(lambda _: ulid.new().str)
+    name = faker('name')
+    username = faker('user_name')
+    email = faker('company_email')
+    password = faker('name')
+    is_active = True
+
