6228f29d7fbf15770c7c650ce730fdaaba56e1ef
Hugo Belém <belem.hugo@hotmail.com>
2024-12-26 15:08:55 -0300

    added contacts model test

--- backend/modules/contacts/tests/test_models.py
+++ backend/modules/contacts/tests/test_models.py
@@ -1,6 +1,16 @@
 from django.test import TestCase
-from django.contrib.auth import get_user_model
-from django.db.utils import IntegrityError
+
+from modules.contacts.models import Contacts
 
 
-
+class TestContactsModel(TestCase):
+    def setUp(self):
+        self.customer = Contacts.objects.create(
+         name='Customer',
+         code='1',
+         entityType='PERSON',
+         contactType='CUSTOMER',   
+        )
+    
+    def test_str_return(self):
+        self.assertEqual(self.customer.__str__(), 'Customer')
