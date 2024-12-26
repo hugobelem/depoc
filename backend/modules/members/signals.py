f0db24a04e2558792dbd6ad2c9a097d827df3504
Hugo Belém <belem.hugo@hotmail.com>
2024-12-25 21:32:15 -0300

    updated members signals

--- backend/modules/members/signals.py
+++ backend/modules/members/signals.py
@@ -1,17 +1,17 @@
-from django.db.models.signals import pre_delete
+from django.db.models.signals import post_delete
 from django.dispatch import receiver
 
 from .models import Members
 
 
-@receiver(pre_delete, sender=Members)
+@receiver(post_delete, sender=Members)
 def delete_member_credentials(sender, instance, **kwargs):
     member = instance
-    if hasattr(member, 'credentials') and member.credentials:
+    if hasattr(member, 'member_credentials'):
         try:
-            members_credentials = member.credentials
-            credentials = members_credentials.credentials
-            credentials.delete()
+            members_credentials = member.member_credentials
+            credential = members_credentials.credential
+            credential.delete()
         except Exception as e:
             # Exception could be logged
             pass
