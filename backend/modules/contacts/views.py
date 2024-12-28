203c067ad4fdca65c593d2a1cfd4e1227c841ca0
Hugo Belém <belem.hugo@hotmail.com>
2024-12-28 19:18:49 -0300

    added patch method to ContactsEndpoint

--- backend/modules/contacts/views.py
+++ backend/modules/contacts/views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 
 from . import services
 from .throttling import BurstRateThrottle, SustainedRateThrottle
-from .serializers import ContactsSerializer
+from .serializers import ContactSerializer
 
 
 class ContactsEndpoint(APIView):
@@ -23,12 +23,10 @@ class ContactsEndpoint(APIView):
             error_response = data
             return error_response
 
-        field_errors = services.check_field_errors(request)
-        if isinstance(field_errors, Response):
-            error_response = field_errors
-            return error_response
+        if field_errors := services.check_field_errors(request):
+            return field_errors
         
-        serializer = ContactsSerializer(data=data, context={'business': business})
+        serializer = ContactSerializer(data=data, context={'business': business})
         if not serializer.is_valid():
             return Response(
                 {'error': 'Validation failed', 'details': serializer.errors},
@@ -55,9 +53,49 @@ class ContactsEndpoint(APIView):
             if not contacts:
                 message = 'Contact not found.'
                 return Response({'error':message}, status=status.HTTP_404_NOT_FOUND)
-            serializer = ContactsSerializer(contacts.contact)
+            serializer = ContactSerializer(contacts.contact)
         else:
             contacts = [bc.contact for bc in business_contacts]
-            serializer = ContactsSerializer(contacts, many=True)
+            serializer = ContactSerializer(contacts, many=True)
 
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+    def patch(self, request, id):
+        business = services.get_business(request)
+        if isinstance(business, Response):
+            error_response = business
+            return error_response
+
+        data = services.get_data(request)
+        if isinstance(data, Response):
+            error_response = data
+            return error_response
+
+        if field_errors := services.check_field_errors(request):
+            return field_errors
+
+        business_contacts = services.get_business_contacts(business)
+        if isinstance(business_contacts, Response):
+            error_response = business_contacts
+            return error_response
+
+        contacts = business_contacts.filter(contact__id=id).first()
+        if not contacts:
+            message = 'Contact not found.'
+            return Response({'error': message}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = ContactSerializer(
+            instance=contacts.contact,
+            data=data,
+            partial=True
+            )
+        
+        if not serializer.is_valid():
+            return Response(
+                {'error': 'Validation failed', 'details': serializer.errors},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
