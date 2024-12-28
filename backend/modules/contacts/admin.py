7c592984f837b1d0d3b731c29c9a4b477874d5bf
Hugo Belém <belem.hugo@hotmail.com>
2024-12-28 19:17:05 -0300

    added id to ContactsAdmin list_display

--- backend/modules/contacts/admin.py
+++ backend/modules/contacts/admin.py
@@ -4,7 +4,7 @@ from .models import Contacts
 
 
 class ContactsAdmin(admin.ModelAdmin):
-    list_display = ('name', 'alias', 'code', 'entityId')
+    list_display = ('name', 'alias', 'code', 'entityId', 'id')
 
 
 admin.site.register(Contacts, ContactsAdmin)
