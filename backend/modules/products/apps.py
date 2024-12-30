34d49a78c0c61de8522a67b8143643465f490f61
Hugo Belém <belem.hugo@hotmail.com>
2024-12-30 12:53:20 -0300

    added verbose_name to products apps.py

--- backend/modules/products/apps.py
+++ backend/modules/products/apps.py
@@ -5,3 +5,4 @@ class ProductsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'modules.products'
     label = 'modules_products'
+    verbose_name = 'Products'
