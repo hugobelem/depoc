17f12cdd7ee0746d567350af4f33f4cb4382bd0a
Hugo Belém <belem.hugo@hotmail.com>
2025-01-02 14:45:29 -0300

    removed unused imports products admin

--- backend/modules/products/admin.py
+++ backend/modules/products/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Products, Category, CostHistory, ProductSupplier
+from .models import Products, Category, CostHistory
 
 
 class ProductsAdmin(admin.ModelAdmin):
