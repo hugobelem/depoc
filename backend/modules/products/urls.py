6120885830e257e31af6079665d1d72877953f04
Hugo Belém <belem.hugo@hotmail.com>
2025-01-02 18:36:42 -0300

    added product CostHistory url paths

--- backend/modules/products/urls.py
+++ backend/modules/products/urls.py
@@ -6,6 +6,8 @@ from . import views
 urlpatterns = [
     path('/categories', views.ProductCategoryEndpoint.as_view()),
     path('/categories/<str:id>', views.ProductCategoryEndpoint.as_view()),
+    path('/costs', views.ProductCostHistoryEndpoint.as_view()),
+    path('/costs/<str:id>', views.ProductCostHistoryEndpoint.as_view()),
     path('', views.ProductsEndpoint.as_view()),
     path('/<str:id>', views.ProductsEndpoint.as_view()),
 ]
