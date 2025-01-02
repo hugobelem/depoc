098811e03848f68db69fd77877f4950429274acb
Hugo Belém <belem.hugo@hotmail.com>
2025-01-02 16:01:16 -0300

    added categories url path

--- backend/modules/products/urls.py
+++ backend/modules/products/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.ProductsEndpoint.as_view()),
-    path('/<str:id>', views.ProductsEndpoint.as_view()) 
+    path('/<str:id>', views.ProductsEndpoint.as_view()),
+    path('/categories', views.ProductCategoryEndpoint.as_view()),
+    path('/categories/<str:id>', views.ProductCategoryEndpoint.as_view()),
 ]
