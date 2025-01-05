45bbfb717583b066cadff7e1b90614295010f383
Hugo Belém <belem.hugo@hotmail.com>
2025-01-05 14:20:54 -0300

    added product_id to ProductCostHistoryEndpoint

--- backend/modules/products/urls.py
+++ backend/modules/products/urls.py
@@ -6,8 +6,14 @@ from . import views
 urlpatterns = [
     path('/categories', views.ProductCategoryEndpoint.as_view()),
     path('/categories/<str:id>', views.ProductCategoryEndpoint.as_view()),
-    path('/costs', views.ProductCostHistoryEndpoint.as_view()),
-    path('/costs/<str:id>', views.ProductCostHistoryEndpoint.as_view()),
+    path(
+        '/<str:product_id>/costs',
+        views.ProductCostHistoryEndpoint.as_view()
+    ),
+    path(
+        '/<str:product_id>/costs/<str:id>',
+        views.ProductCostHistoryEndpoint.as_view()
+    ),
     path('', views.ProductsEndpoint.as_view()),
     path('/<str:id>', views.ProductsEndpoint.as_view()),
 ]
