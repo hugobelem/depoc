347431170880b82bd2beb9a17ba03984e0303949
Hugo Belém <belem.hugo@hotmail.com>
2025-01-05 19:45:37 -0300

    imported all animals models to admin

--- backend/modules/animals/admin.py
+++ backend/modules/animals/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
+from django.contrib.admin import register
 
-from .models import Animals
+from .models import (
+    Animal,
+    AnimalFinancial,
+    AnimalLifeCycle,
+    AnimalGrowth,
+    AnimalWeight,
+    AnimalMeatQuality,
+    AnimalHealth,
+)
 
+@register(Animal)
+class AnimalAdmin(admin.ModelAdmin):
+    list_display = ['tagNumber', 'species', 'status', 'id']
 
-class AnimalsAdmin(admin.ModelAdmin):
-    list_display = ['species', 'breed', 'gender', 'arrivalDate', 'weight']
-
-
-admin.site.register(Animals, AnimalsAdmin)
