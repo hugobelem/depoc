2e89f327df07dff68026179023f45c9c6785eba1
Hugo Belém <belem.hugo@hotmail.com>
2025-01-05 19:54:34 -0300

    registed animals models to admin

--- backend/modules/animals/admin.py
+++ backend/modules/animals/admin.py
@@ -15,3 +15,32 @@ from .models import (
 class AnimalAdmin(admin.ModelAdmin):
     list_display = ['tagNumber', 'species', 'status', 'id']
 
+@register(AnimalFinancial)
+class AnimalFinancialAdmin(admin.ModelAdmin):
+    list_display = ['animal', 'purchaseCost', 'maintenanceCost', 'estimatedValue']
+
+@register(AnimalLifeCycle)
+class AnimalLifeCycleAdmin(admin.ModelAdmin):
+    list_display = ['animal', 'arrivalDate', 'processedDate']
+
+@register(AnimalGrowth)
+class AnimalGrowthAdmin(admin.ModelAdmin):
+    list_display = ['animal', 'feedType', 'feedConsumed', 'GrowthRate']
+
+@register(AnimalWeight)
+class AnimalWeightAdmin(admin.ModelAdmin):
+    list_display = [
+        'animal',
+        'weight',
+        'processedWeight',
+        'carcassWeight',
+        'dressingPercentage',
+    ]
+
+@register(AnimalMeatQuality)
+class AnimalMeatQualityAdmin(admin.ModelAdmin):
+    list_display = ['animal', 'backFatThickness']
+
+@register(AnimalHealth)
+class AnimalHealthAdmin(admin.ModelAdmin):
+    list_display = ['animal', 'healthStatus', 'notes']
