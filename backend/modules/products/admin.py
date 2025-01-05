863f4a3e378793dd5edc449c36c88bdb608f127a
Hugo Belém <belem.hugo@hotmail.com>
2025-01-05 20:40:26 -0300

    registered ProductAnimal to admin

--- backend/modules/products/admin.py
+++ backend/modules/products/admin.py
@@ -1,16 +1,17 @@
 from django.contrib import admin
+from django.contrib.admin import register
 
-from .models import Products, Category, CostHistory
+from .models import Products, Category, CostHistory, ProductAnimal
 
-
+@register(Products)
 class ProductsAdmin(admin.ModelAdmin):
-    list_display = ('name', 'sku', 'unit', 'retailPrice', 'id')
+    list_display = ['name', 'sku', 'unit', 'retailPrice', 'id']
 
-
+@register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name']
 
-
+@register(CostHistory)
 class CostHistoryAdmin(admin.ModelAdmin):
     list_display = [
         'product',
@@ -22,7 +23,6 @@ class CostHistoryAdmin(admin.ModelAdmin):
         'markup',
     ]
 
-
-admin.site.register(Products, ProductsAdmin)
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(CostHistory, CostHistoryAdmin)
+@register(ProductAnimal)
+class ProductAnimalAdmin(admin.ModelAdmin):
+    list_display = ['animal', 'product', 'packagingDate', 'expirationDate']
