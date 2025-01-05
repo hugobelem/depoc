0f9c45570ec7a029daffc0d44deb5b959cd5596a
Hugo Belém <belem.hugo@hotmail.com>
2025-01-05 17:56:18 -0300

    registed Animals to admin

--- backend/modules/animals/admin.py
+++ backend/modules/animals/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Animals
+
+
+class AnimalsAdmin(admin.ModelAdmin):
+    list_display = ['species', 'breed', 'gender', 'arrivalDate', 'weight']
+
+
+admin.site.register(Animals, AnimalsAdmin)
