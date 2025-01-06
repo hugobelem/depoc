3f72e9a992d0a8bc9f717ee453ff88e533e784b1
Hugo Belém <belem.hugo@hotmail.com>
2025-01-06 19:41:59 -0300

    added signals to update inventory quantity

--- backend/modules/inventory/signals.py
+++ backend/modules/inventory/signals.py
@@ -1,4 +1,6 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_delete
+from django.db import transaction
+from django.db.models import Sum
 from django.dispatch import receiver
 
 import ulid
@@ -12,3 +14,27 @@ def generate_ulid(sender, instance, **kwargs):
     if not instance.id:
         instance.id = ulid.new().str
  
+
+@receiver(pre_save, sender=InventoryTransaction)
+def update_inventory_quantity(sender, instance, **kwargs):
+    with transaction.atomic():
+        inventory = instance.inventory
+
+        total_quantity = InventoryTransaction.objects\
+            .filter(inventory=inventory)\
+            .aggregate(Sum('quantity'))['quantity__sum'] or 0
+
+        inventory.quantity = total_quantity
+        inventory.save()
+
+@receiver(post_delete, sender=InventoryTransaction)
+def update_inventory_quantity_on_delete(sender, instance, **kwargs):
+    with transaction.atomic():
+        inventory = instance.inventory
+
+        total_quantity = InventoryTransaction.objects\
+            .filter(inventory=inventory)\
+            .aggregate(Sum('quantity'))['quantity__sum'] or 0
+
+        inventory.quantity = total_quantity
+        inventory.save()
