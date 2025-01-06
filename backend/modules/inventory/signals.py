9c736daecc1e0c1f1a20945b5b961ca4855caf3f
Hugo Belém <belem.hugo@hotmail.com>
2025-01-06 18:28:30 -0300

    added InventoryTransaction to signals

--- backend/modules/inventory/signals.py
+++ backend/modules/inventory/signals.py
@@ -3,10 +3,12 @@ from django.dispatch import receiver
 
 import ulid
 
-from .models import Inventory
+from .models import Inventory, InventoryTransaction
 
 
 @receiver(pre_save, sender=Inventory)
+@receiver(pre_save, sender=InventoryTransaction)
 def generate_ulid(sender, instance, **kwargs):
     if not instance.id:
         instance.id = ulid.new().str
+ 
