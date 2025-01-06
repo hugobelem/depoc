ae55469b58c3df1d23096fd51e8a398c99f5fac3
Hugo Belém <belem.hugo@hotmail.com>
2025-01-06 18:27:05 -0300

    added inventory models to admin

--- backend/modules/inventory/admin.py
+++ backend/modules/inventory/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.admin import register
 
-# Register your models here.
+from .models import Inventory, InventoryTransaction
+
+
+@register(Inventory)
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = ['product', 'quantity']
+
+
+@register(InventoryTransaction)
+class InventoryTransactionAdmin(admin.ModelAdmin):
+    list_display = ['transactionType', 'quantity', 'unitCost']
