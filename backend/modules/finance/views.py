1ffa3f5f400b0b1000c38fcc5b7fca76d5b7ac05
Hugo Belém <belem.hugo@hotmail.com>
2025-01-07 19:42:46 -0300

    added BankAccountEndpoint view

--- backend/modules/finance/views.py
+++ backend/modules/finance/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import permissions
+from rest_framework import status
 
-# Create your views here.
+from . import services
+from .throttling import BurstRateThrottle, SustainedRateThrottle
+
+
+class BankAccountEndpoint(APIView):
+    permission_classes = [permissions.IsAdminUser]
+    throttle_classes = [BurstRateThrottle, SustainedRateThrottle]
