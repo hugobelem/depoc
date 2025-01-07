f1c9040f052ac7ddb55a03caf234ff59753d7f02
Hugo Belém <belem.hugo@hotmail.com>
2025-01-07 20:26:08 -0300

    added BankAccountEndpoint post method

--- backend/modules/finance/views.py
+++ backend/modules/finance/views.py
@@ -5,8 +5,38 @@ from rest_framework import status
 
 from . import services
 from .throttling import BurstRateThrottle, SustainedRateThrottle
+from .serializers import BankAccountSerializer
 
 
 class BankAccountEndpoint(APIView):
     permission_classes = [permissions.IsAdminUser]
     throttle_classes = [BurstRateThrottle, SustainedRateThrottle]
+
+    def post(self, request):
+        business = services.get_business(request)
+        if isinstance(business, Response):
+            error_response = business
+            return error_response
+        
+        data = services.get_data(request)
+        if isinstance(data, Response):
+            error_response = data
+            return error_response
+        
+        if field_errors := services.check_field_errors(
+                request,
+                BankAccountSerializer,
+            ):
+            return field_errors
+        
+        serializer = BankAccountSerializer(data=data)
+
+        if not serializer.is_valid():
+            message = 'Validation failed: '
+            return Response(
+                {'error': message, 'details': serializer.errors},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
