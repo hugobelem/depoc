f0185c3f3b59f8e61cd4d654791317947dd351e1
Hugo Belém <belem.hugo@hotmail.com>
2025-01-07 10:31:30 -0300

    added expected_fields to InventorySerializer

--- backend/modules/inventory/serializers.py
+++ backend/modules/inventory/serializers.py
@@ -6,8 +6,9 @@ from .models import Inventory, InventoryTransaction
 class InventorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Inventory
-        fields = '__all__'
+        fields = ['product', 'quantity', 'reserved', 'location']
         read_only_fields = ['product', 'quantity', 'reserved']
+        expected_fields = ['location']
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
