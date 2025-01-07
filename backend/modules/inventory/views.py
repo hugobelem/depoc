505e1b0077696f3ec70d70a5caa78935236c8307
Hugo Belém <belem.hugo@hotmail.com>
2025-01-07 10:05:17 -0300

    added InventoryEndpoint view

--- backend/modules/inventory/views.py
+++ backend/modules/inventory/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import permissions
+from rest_framework import status
 
-# Create your views here.
+from . import services
+from .throttling import BurstRateThrottle, SustainedRateThrottle
+from .serializers import InventorySerializer
+from .models import Inventory
+
+class InventoryEndpoint(APIView):
+    permission_classes = [permissions.IsAdminUser]
+    throttle_classes = [BurstRateThrottle, SustainedRateThrottle]
+
+    def patch(self, request, id):
+        business = services.get_business(request)
+        if isinstance(business, Response):
+            error_response = business
+            return error_response
+
+        business_products = services.get_business_products(business)
+        if isinstance(business, Response):
+            error_response = business_products
+            return error_response
+        
+        data = services.get_data(request)
+        if isinstance(business, Response):
+            error_response = data
+            return error_response
+        
+        if field_errors := services.check_field_errors(request, InventorySerializer):
+            return field_errors
+        
+        inventory = Inventory.objects.filter(id=id).first()
+        serializer = InventorySerializer(instance=inventory, data=data, partial=True)
+
+        if not serializer.is_valid():
+            return Response(
+                {'error': 'Validation failed', 'details': serializer.errors},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+g
