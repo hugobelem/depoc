f377f7c6d1684e91cd6af3fae9246573d0b617db
Hugo Belém <belem.hugo@hotmail.com>
2025-01-07 21:40:38 -0300

    added BankAccountEndpoint patch method

--- backend/modules/finance/views.py
+++ backend/modules/finance/views.py
@@ -64,3 +64,44 @@ class BankAccountEndpoint(APIView):
             serializer = BankAccountSerializer(bank_accounts, many=True)
 
         return Response(serializer.data, status.HTTP_200_OK)
+
+
+    def patch(self, request, id):
+        business = services.get_business(request)
+        if isinstance(business, Response):
+            error_response = business
+            return error_response
+        
+        data = services.get_data(request)
+        if isinstance(data, Response):
+            error_response = data
+            return error_response
+        
+        if field_errors := services.check_field_errors(
+                request,
+                BankAccountSerializer,
+            ):
+            return field_errors
+        
+        business_banks = services.get_business_banks(business)
+
+        bank_account = business_banks.filter(bankAccount__id=id).first()
+        if not bank_account:
+            message = 'Bank Account not found.'
+            return Response({'error': message}, status.HTTP_404_NOT_FOUND)
+
+        serializer = BankAccountSerializer(
+            instance=bank_account.bankAccount,
+            data=data,
+            partial=True,
+        )
+
+        if not serializer.is_valid():
+            message = 'Validation failed: '
+            return Response(
+                {'error': message, 'details': serializer.errors},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        serializer.save()
+        return Response(serializer.data, status.HTTP_200_OK)
