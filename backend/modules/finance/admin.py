373c3b8f7e6bfa5e330b848b18e775210fadadc5
Hugo Belém <belem.hugo@hotmail.com>
2025-01-09 09:00:02 -0300

    registered finance category in admin

--- backend/modules/finance/admin.py
+++ backend/modules/finance/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.contrib.admin import register
 
-from .models import BankAccount
+from .models import BankAccount, Category
 
 @register(BankAccount)
 class BankAccountAdmin(admin.ModelAdmin):
     list_display = ['bankName', 'accountNumber', 'balance', 'status']
+
+@register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'parent', 'status']
