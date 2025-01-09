852e43c0cf3e9a011ba9db5935c116e9ba9579eb
Hugo Belém <belem.hugo@hotmail.com>
2025-01-09 09:38:50 -0300

    added CategoryEndpoint post method

--- backend/modules/finance/views.py
+++ backend/modules/finance/views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 
 from . import services
 from .throttling import BurstRateThrottle, SustainedRateThrottle
-from .serializers import BankAccountSerializer
+from .serializers import BankAccountSerializer, CategorySerializer
 
 
 class BankAccountEndpoint(APIView):
@@ -125,3 +125,40 @@ class BankAccountEndpoint(APIView):
         bank_account.save()
 
         return Response({'success': 'Bank Account deleted.'}, status.HTTP_200_OK)
+
+
+class CategoryEndpoint(APIView):
+    permission_classes = [permissions.IsAdminUser]
+    throttle_classes = [BurstRateThrottle, SustainedRateThrottle]
+
+    def post(self, request):
+        business = services.get_business(request)
+        if isinstance(business, Response):
+            error_response = business
+            return error_response
+        
+        data = services.get_data(request)
+        if isinstance(data, Response):
+            error_response = data
+            return error_response
+        
+        if field_errors := services.check_field_errors(
+                request,
+                CategorySerializer,
+            ):
+            return field_errors
+        
+        serializer = CategorySerializer(
+            data=data,
+            context={'business': business},
+        )
+
+        if not serializer.is_valid():
+            message = 'Validation failed: '
+            return Response(
+                {'error': message, 'details': serializer.errors},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
