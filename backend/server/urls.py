ef385d2b8f83b95e36a818b3c69537b81ed5b1cb
Hugo Belém <belem.hugo@hotmail.com>
2025-01-09 09:30:44 -0300

    added finance path to finance app

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('products', include('modules.inventory.urls')),
     path('products', include('modules.products.urls')),
     path('accounts', include('modules.finance.urls')),
+    path('finance', include('modules.finance.urls')),
     path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
