f7d84884fb8ba72246754a9e2a5294f8631386ca
Hugo Belém <belem.hugo@hotmail.com>
2025-01-09 09:00:59 -0300

    added finance category to signals

--- backend/modules/finance/signals.py
+++ backend/modules/finance/signals.py
@@ -3,9 +3,10 @@ from django.dispatch import receiver
 
 import ulid
 
-from .models import BankAccount
+from .models import BankAccount, Category
 
 @receiver(pre_save, sender=BankAccount)
+@receiver(pre_save, sender=Category)
 def generate_ulids(sender, instance, **kwargs):
     if not instance.id:
         instance.id = ulid.new().str
