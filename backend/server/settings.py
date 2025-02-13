e0c4712787fd8308040455d9af8064fd388a3ebd
Hugo Belém <belem.hugo@hotmail.com>
2025-02-13 20:06:57 -0300

    changed to postgres db

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -102,21 +102,13 @@ TEMPLATES = [
 ]
 
 
-if ENVIRONMENT == 'production':
-    database_url = os.environ.get('DATABASE_URL')
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=database_url,
-            conn_max_age=600
-        )
-    }
-else: 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+database_url = os.environ.get('DATABASE_URL')
+DATABASES = {
+    'default': dj_database_url.config(
+        default=database_url,
+        conn_max_age=600
+    )
+}
 
 
 AUTH_PASSWORD_VALIDATORS = [
