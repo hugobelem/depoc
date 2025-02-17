f45acd1fb2af1a8835d686ef98c82e9baee7ea50
Hugo Belém <belem.hugo@hotmail.com>
2025-02-17 13:10:11 -0300

    update user throttling rates

--- backend/shared/throttling.py
+++ backend/shared/throttling.py
@@ -3,9 +3,9 @@ from rest_framework.throttling import UserRateThrottle
 
 class BurstRateThrottle(UserRateThrottle):
     scope = 'burst'
-    rate = '10/min'
+    rate = '60/min'
 
 
 class SustainedRateThrottle(UserRateThrottle):
     scope = 'sustained'
-    rate = '50/day'
+    rate = '500/day'
