6b728c5e3238725772534d57e25d75eda6db66dd
Hugo Belém <belem.hugo@hotmail.com>
2025-03-20 17:16:27 -0300

    changed unknown exception msg

--- backend/core/exceptions.py
+++ backend/core/exceptions.py
@@ -3,15 +3,12 @@ from rest_framework.response import Response
 from rest_framework import status
 
 import os
-import logging
 
 
-logger = logging.getLogger(__name__)
 is_production = os.getenv('ENVIRONMENT') == 'production'
 
 def handler(exc, context):
     response = exception_handler(exc, context)
-    logger.error(f'EXCEPTION OCCURRED: {response}')
 
     if response:
         data = {
@@ -23,11 +20,10 @@ def handler(exc, context):
         return Response(data, status=response.status_code)
     
     elif not response and is_production:
-        logger.error(f'EXCEPTION IN PROD OCCURRED: {response}')
         data = {
             'error': {
                 'status': 500,
-                'message': 'An error occurred.',
+                'message': 'An unexpected error occurred.',
             }
         }
         return Response(data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
