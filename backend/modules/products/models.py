2fbb5414fc3bde6792d0e8914be5c4c61de6c3b5
Hugo Belém <belem.hugo@hotmail.com>
2025-05-14 20:00:58 -0300

    fix: product model cost_price blank & null

--- backend/modules/products/models.py
+++ backend/modules/products/models.py
@@ -46,7 +46,8 @@ class Product(models.Model):
     cost_price = models.DecimalField(
 	    max_digits=10,
         decimal_places=2,
-        default=0
+        blank=True,
+        null=True,
 	)
     
     retail_price = models.DecimalField(
