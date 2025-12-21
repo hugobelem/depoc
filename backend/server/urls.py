264cc19c3020708985fcad766165770f81f1ee76
Hugo Belém <belem.hugo@hotmail.com>
2025-12-21 19:40:03 +0000

    feat: add access_token to HTTP-only cookie

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -2,7 +2,38 @@ from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
 
+class CookieTokenObtainPairView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.user
+        refresh = RefreshToken.for_user(user)
+        access_token = str(refresh.access_token)
+        refresh_token = str(refresh)
+
+        response = Response({
+            "access": access_token,
+            "refresh": refresh_token
+        })
+        response.set_cookie(
+            key="access_token",
+            value=access_token,
+            httponly=True,
+            secure=True,
+            samesite='Lax',
+        )
+        response.set_cookie(
+            key="refresh_token",
+            value=refresh_token,
+            httponly=True,
+            secure=True,
+            samesite='Lax',
+        )
+
+        return response
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,6 +47,6 @@ urlpatterns = [
     path('products', include('modules.products.urls')),
     path('', include('modules.billing.urls')),
 
-    path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token', CookieTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
