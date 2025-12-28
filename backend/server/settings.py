db77f3eff08c4fb61fa6edf11cfd710ffe26fafe
Hugo Belém <belem.hugo@hotmail.com>
2025-12-28 18:47:06 +0100

    test: put cors middleware at the top

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -78,6 +78,7 @@ SIMPLE_JWT = {
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     # 'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -85,7 +86,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 
