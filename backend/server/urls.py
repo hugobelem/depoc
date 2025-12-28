841e516da580a04cd444abd1f81cf306c6391a96
Hugo Belém <belem.hugo@hotmail.com>
2025-12-28 18:15:53 +0100

    fix: server does not send http-only jwt

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -2,46 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework.response import Response
-
-from .settings import ENVIRONMENT
-
-secure = ENVIRONMENT != 'development'
-
-class CookieTokenObtainPairView(TokenObtainPairView):
-    def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.user
-        refresh = RefreshToken.for_user(user)
-        access_token = str(refresh.access_token)
-        refresh_token = str(refresh)
-
-        response = Response({
-            "access": access_token,
-            "refresh": refresh_token
-        })
-        response.set_cookie(
-            key="access_token",
-            value=access_token,
-            httponly=True,
-            path="/",
-            max_age=3600,
-            secure=secure,
-            samesite="None",
-        )
-        response.set_cookie(
-            key="refresh_token",
-            value=refresh_token,
-            httponly=True,
-            path="/",
-            max_age=3600,
-            secure=secure,
-            samesite="None",
-        )
-
-        return response
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -55,6 +15,6 @@ urlpatterns = [
     path('products', include('modules.products.urls')),
     path('', include('modules.billing.urls')),
 
-    path('token', CookieTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
